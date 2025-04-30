8909b8dfed5553b264650fd8c0374ffffe5ca2b2
Mohammad <mhz0110747488@gmail.com>
2025-04-30 08:48:08 +0330

    write yhe product class

---
+++ src/Products/Product.java
@@ -0,0 +1,30 @@
+package Products;
+
+public class Product {
+    private String title;
+    private Double price;
+    private String id;
+    private static int productCount = 0;
+
+    public Product(String title, Double price) {
+        this.title = title;
+        this.price = price;
+        this.id = generateId();
+        productCount++;
+    }
+
+    public String getTitle() { return title; }
+    public void setTitle(String title) { this.title = title; }
+
+    public Double getPrice() { return price; }
+    public void setPrice(Double price) { this.price = price; }
+
+    public String getId() { return id; }
+
+    public String toString() {
+        return "ID: " + id + ", Title: " + title + ", Price: " + price;
+    }
+
+    protected abstract String generateId();
+
+}
